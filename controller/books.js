7da72fe072066572b60bd73bc42506ebb79bbe1e
Valentin Sorohan <valentinsorohan@gmail.com>
2024-09-14 00:00:10 +0200

    feat: :sparkles: add logic to get books from db

--- controller/books.js
+++ controller/books.js
@@ -1,21 +1,26 @@
-import books from "../model/supabase.js";
+import supabase from "../model/supabase.js";
 
 export function getBookById(req, res) {
-  const bookId = +req.params.bookId;
-  const book = books.find((book) => book.id === bookId);
-  if (book) {
-    res.json(book);
-  } else {
-    res.json({ page: "Not Found" });
-  }
+  // const bookId = +req.params.bookId;
+  // const book = books.find((book) => book.id === bookId);
+  // if (book) {
+  //   res.json(book);
+  // } else {
+  //   res.json({ page: "Not Found" });
+  // }
 }
-
-export function getBooks(req, res, next) {
-  res.json(books);
+export async function getBooks(req, res, next) {
+  const { data, error } = await supabase.from("libro").select("*");
+  if (error) {
+    console.error("Error fetching data:", error);
+  } else {
+    console.log("Data:", data);
+  }
+  res.json(data);
 }
 
 export function postBook(req, res) {
-  const book = req.body;
-  console.log(book);
-  res.json(book);
+  // const book = req.body;
+  // console.log(book);
+  // res.json(book);
 }
