be6928f71b481369a21a5ceef8206d85b05a9135
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-09-14 18:09:30 +0200

    fix: replace BookId with BookIsbn
    replaced id with isbn since the book db table has no "id" column and
    uses the isbn instead

--- controller/books.js
+++ controller/books.js
@@ -10,16 +10,19 @@ export async function getBooks(_, res) {
   res.json(data);
 }
 
-export async function getBookById(req, res) {
-  const bookId = +req.params.bookId;
-  const { data, error } = await supabase.from("book").select().eq("id", bookId);
+export async function getBookByIsbn(req, res) {
+  const bookIsbn = req.params.bookIsbn;
+  const { data, error } = await supabase
+    .from("book")
+    .select("*")
+    .eq("isbn", bookIsbn);
 
   if (error) {
     console.error("Error fetching data:", error);
   } else {
     console.log("Data:", data);
   }
-  res.json(data);
+  res.json(data.length === 1 ? data[0] : data);
 }
 
 export async function postBook(req, res) {
